d38d30a08fcd32fcb4fa704ab174390861a4603e
Carlos Moura <carlosmourapires88@gmail.com>
2022-01-26 18:48:47 -0300

    Requisito 6 concluido

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,26 @@
 const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  let child = 0;
+  let adult = 0;
+  let senior = 0;
+  entrants.forEach((element) => {
+    if (element.age < 18) {
+      child += 1;
+    } else if (element.age < 50) {
+      adult += 1;
+    } else senior += 1;
+  });
+  return { child, adult, senior };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) return 0;
+  const entradas = countEntrants(entrants);
+  const { child, adult, senior } = entradas;
+  const valorTotal = child * prices.child + adult * prices.adult + senior * prices.senior;
+  return valorTotal;
 }
 
 module.exports = { calculateEntry, countEntrants };
