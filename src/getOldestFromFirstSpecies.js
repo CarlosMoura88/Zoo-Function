a5cfeb8bef6cf6fd20c0139dc20ea3ae13c822a7
Carlos Moura <carlosmourapires88@gmail.com>
2022-01-26 19:30:32 -0300

    Requisito 9 concluido

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const funcionario = employees.find((employee) => employee.id === id);
+  const pegaBicho = species.find((specie) => specie.id === funcionario.responsibleFor[0]);
+  const meuBichoVelho = pegaBicho.residents.sort((a, b) => b.age - a.age);
+  const { name, sex, age } = meuBichoVelho[0];
+  return [name, sex, age];
 }
+
+console.log(getOldestFromFirstSpecies('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 
 module.exports = getOldestFromFirstSpecies;
