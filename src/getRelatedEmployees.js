2154a9612a16900faaf8ee8b1dabf1b6ba4b512c
Carlos Moura <carlosmourapires88@gmail.com>
2022-01-25 21:11:00 -0300

    Requisito 4 concluido

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,19 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const retorno = [];
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  employees.forEach((el) => {
+    if (el.managers.includes(managerId)) retorno.push(`${el.firstName} ${el.lastName}`);
+  });
+  return retorno;
 }
 
 module.exports = { isManager, getRelatedEmployees };
