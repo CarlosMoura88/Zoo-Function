ae06a90985c1075ceb0d05bead875d3bff56005c
Carlos Moura <carlosmourapires88@gmail.com>
2022-01-26 00:07:36 -0300

    Requisito 5 concluido

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    return species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+  }
+  const bichinho = species.find((specie) => specie.name === animal.specie);
+  if (animal.sex === undefined) return bichinho.residents.length;
+  const resultado = bichinho.residents.filter((el) => el.sex === animal.sex);
+  return resultado.length;
 }
+
+console.log(countAnimals({ specie: 'giraffes', sex: 'female' }));
 
 module.exports = countAnimals;
